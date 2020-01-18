f6ba5e720625d59fae6d82a2ecc6b06c7f56e255
randima-dissanayake <randimadissanayake.rkd@gmail.com>
2020-01-18 22:14:46 +0530

    Maven project

---
+++ Maven/MainProject/src/main/java/Application.java
@@ -0,0 +1,8 @@
+import com.mycompany.app.App;
+
+public class Application {
+    public static void main(String[] args) {
+        App app=new App();
+        app.print();
+    }
+}
---
+++ Maven/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void print(){
+        System.out.println("Hi I'm the sub class");
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
