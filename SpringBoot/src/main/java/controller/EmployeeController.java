6440494139a777fbdb94215ef8ad65e043ab8879
randima-dissanayake <randimadissanayake.rkd@gmail.com>
2020-02-07 01:07:50 +0530

    Spring boot simple project

---
+++ SpringBoot/src/main/java/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/services")
+@XmlRootElement
+public class EmployeeController {
+    @RequestMapping("/greeting")
+    public void greeting(){
+        System.out.println("Hello");
+    }
+
+    @RequestMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return Employee.getAllEmployees();
+    }
+
+}
---
+++ SpringBoot/src/main/java/model/Employee.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private int mark;
+
+    Employee(String name, int mark){
+        this.name=name;
+        this.mark=mark;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return mark;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(int mark) {
+        this.mark = mark;
+    }
+
+    public static List<Employee> getAllEmployees(){
+        List<Employee> employeeList=new ArrayList<>();
+        employeeList.add(new Employee("randima", 70));
+        employeeList.add(new Employee("thilini",12));
+        employeeList.add(new Employee("primali",56));
+        employeeList.add(new Employee("hansika",87));
+
+        return employeeList;
+    }
+}
