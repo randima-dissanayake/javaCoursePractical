b19b78fa47e3ccab5d84d57b42388c9c5b4470d8
randima-dissanayake <randimadissanayake.rkd@gmail.com>
2020-02-25 23:17:41 +0530

    HTTP Chat application command for register

--- HTTPChat/Client/src/Client.java
+++ HTTPChat/Client/src/Client.java
@@ -7,20 +7,30 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        System.out.println("Enter command:");
+
         Scanner scanner=new Scanner(System.in);
-        String name = scanner.nextLine();
+        String line = scanner.nextLine();
 
-        URL url=new URL("http://localhost:8000/register?name="+name);
+        String[] tokens= line.split(" ");
 
-        HttpURLConnection connection=(HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
+        if ("connect".equalsIgnoreCase(tokens[0])){
+            String name = line.substring(line.indexOf("as ")+1);
+            URL url=new URL("http://localhost:8000/register?name="+name);
+            HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line=reader.readLine())!=null){
-            System.out.println(line);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line1;
+            while ((line1=reader.readLine())!=null){
+                System.out.println(line1);
+            }
+
+            reader.close();
         }
 
-        reader.close();
+
+
+
     }
 }
