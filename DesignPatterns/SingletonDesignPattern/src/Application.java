38c14694a30ba5b15d44f3fecb019cafb012cb48
randima-dissanayake <randimadissanayake.rkd@gmail.com>
2020-05-11 16:46:41 +0530

    Singleton desing pattern

---
+++ DesignPatterns/SingletonDesignPattern/src/Application.java
@@ -0,0 +1,8 @@
+public class Application {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton);
+        Singleton singleton1= Singleton.getInstance();
+        System.out.println(singleton1);
+    }
+}
